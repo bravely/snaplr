11c96f0af04fe21da9923ffb3fd6a083dac381a8
Jake Mays <quaunaut@gmail.com>
2015-03-09 21:25:19 -0700

    Added home page.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @posts = Post.where(user: current_user.followed).order(created_at: :desc).includes(:user)
+    @posts = Post.where(user: current_user.followed).order(created_at: :desc).includes(:user) if current_user
   end
 
   def public
--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,22 +2,32 @@ require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
   describe 'GET #index' do
-    let(:user) { FactoryGirl.create(:user) }
-    let(:post_one) { FactoryGirl.create(:post, created_at: Time.current) }
-    let(:post_two) { FactoryGirl.create(:post, created_at: 1.hour.ago) }
-    let(:post_three) { FactoryGirl.create(:post, created_at: 2.hours.ago) }
-    let(:unfollowed_post) { FactoryGirl.create(:post) }
-    before do
-      sign_in user
-      user.follow!(post_one.user)
-      user.follow!(post_two.user)
-      user.follow!(post_three.user)
-      get :index
+    context 'when a user is signed in' do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:post_one) { FactoryGirl.create(:post, created_at: Time.current) }
+      let(:post_two) { FactoryGirl.create(:post, created_at: 1.hour.ago) }
+      let(:post_three) { FactoryGirl.create(:post, created_at: 2.hours.ago) }
+      let(:unfollowed_post) { FactoryGirl.create(:post) }
+      before do
+        sign_in user
+        user.follow!(post_one.user)
+        user.follow!(post_two.user)
+        user.follow!(post_three.user)
+        get :index
+      end
+      it { should respond_with :ok }
+      it { should render_template :index }
+      it { expect(assigns(:posts)).to eq [post_one, post_two, post_three] }
+      it { expect(assigns(:posts)).to_not include unfollowed_post }
     end
-    it { should respond_with :ok }
-    it { should render_template :index }
-    it { expect(assigns(:posts)).to eq [post_one, post_two, post_three] }
-    it { expect(assigns(:posts)).to_not include unfollowed_post }
+    context 'when a user is not signed in' do
+      before do
+        get :index
+      end
+      it { should respond_with :ok }
+      it { should render_template :index }
+      it { expect(assigns(:posts)).to eq nil }
+    end
   end
 
   describe 'GET #index' do
